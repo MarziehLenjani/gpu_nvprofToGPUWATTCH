43135ed91b6db85b053f0d855e642d8b2ce1876b
Marzieh <marzieh@d-172-26-7-20.dhcp.virginia.edu>
2019-04-28 15:42:09 -0400

    more detail parsing

--- nvProfToGPUWATTCH.py
+++ nvProfToGPUWATTCH.py
@@ -437,9 +437,8 @@ def runAndGetEnergy(xmlFile,dataFrameContainingStats,dataFramContaingExpressions
     NoCenergyStr = 'NoCenergy'
     MCenergyStr = 'MCenergy'
     DRAM_EnergyStr= 'DRAM_EnergyStr'
-    #tmpEnergyDf = pd.DataFrame(columns=[runtimeStr,PenergyStr,CenergyStr,L2energyStr,NoCenergyStr,MCenergyStr])
+    tmpEnergyDf = pd.DataFrame(columns=[runtimeStr,PenergyStr,CenergyStr,L2energyStr,NoCenergyStr,MCenergyStr])
 
-    tmpEnergyDf = pd.DataFrame()
     #leakage, dynamic = runMcPAT(procConfigFile)
     #Pleakage, Pdynamic, Cleakage, Cdynamic, L3leakage, L3dynamic, NoCleakage, NoCdynamic, MCleakage, MCdynamic=0,0,0,0,0,0,0,0,0,0
     Pleakage,Pdynamic,Cleakage,Cdynamic,L3leakage,L3dynamic,NoCleakage,NoCdynamic,MCleakage,MCdynamic=runGPUWATTCH(xmlFile)
