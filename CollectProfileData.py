f9eec4aa98d0dbb007c610c968a65c3232967259
Marzieh <marzieh@d-172-25-98-81.dhcp.virginia.edu>
2019-04-30 14:32:33 -0400

    more accurate merics

--- CollectProfileData.py
+++ CollectProfileData.py
@@ -73,10 +73,10 @@ def main():
 
                         tmpMax=0
                         for j in range(len(dataFrame)):
-                            if ( dataFrame.iloc[i].loc['Max']>tmpMax):
+                            if ((j%4 ==3 ) and dataFrame.iloc[i].loc['Max']>tmpMax):
                                 tmpMax=dataFrame.iloc[i].loc['Max']
 
-                            metricValue=tmpMax
+                        metricValue=tmpMax
 
                     elif(type(dataFrame.iloc[i].loc['Avg']) != str):
                         metricValue +=(dataFrame.iloc[i].loc['Avg'])
